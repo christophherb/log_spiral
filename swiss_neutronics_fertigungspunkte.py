9064daa87313990b8711143cef8c4444b29ff52c
Christoph Herb <herb-c@t-online.de>
2022-03-30 09:18:13 +0200

    revision of evalutation using sliders

---
+++ swiss_neutronics_fertigungspunkte.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import Slider
+from new_log_spir import LogSpir as lgs
+
+#try to plot several log spirals for various lengths, angles of attack and spiral densities, options are variable via slider, once a suitable combination of parameters is found,
+#close the plot window and the resulting 'Fertigungspunk
